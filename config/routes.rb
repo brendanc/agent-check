c66aea39a51b22c940b1716ea1f62aa859fcb414
Brendan <brendan@Devspade.com>
2019-10-05 12:07:22 -0400

    add sample email with tracking image

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 AgentCheck::Application.routes.draw do
 
-  get "current/ip"
-
-  get 'hits/index'
+  get 'current/ip'
+  get 'email' => 'email#index'
+  get 'hits/index' 
   get 'hits/create'
   get 'hits/clear'
   get 'hits/dyn' => 'hits#dynamic_image'
   get 'hits' => 'hits#index'
 
   #Root:
-  root :to => "hits#index"
+  root :to => 'hits#index'
 
 end
