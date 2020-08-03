8cb33ab576dad2e9a4e7b6da11fe1b1f5246fab2
Brendan <brendan@Devspade.com>
2020-08-03 17:50:08 -0400

    re-add the nocache headers

--- app/controllers/hits_controller.rb
+++ app/controllers/hits_controller.rb
@@ -108,9 +108,9 @@ before_action :set_cache_headers
 
   private
     def set_cache_headers
-      # response.headers["Expires"] = ""
-      # response.headers["Cache-Control"] = "no-store, no-cache, must-revalidate, max-age=0, post-check=0, pre-check=0"
-      # response.headers["Pragma"] = "no-cache"     
+      response.headers["Expires"] = "Mon, 01 Jan 1990 00:00:00 GMT"
+      response.headers["Cache-Control"] = "no-store, no-cache, must-revalidate, max-age=0, post-check=0, pre-check=0"
+      response.headers["Pragma"] = "no-cache"     
     end
 
     def send_file_with_content_length(path, options = {})
