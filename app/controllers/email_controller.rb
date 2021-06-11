5d4db623fe0b6f5b3e0d0bba33b924ac93645cc4
Brendan <brendan@Devspade.com>
2021-06-11 15:58:12 -0400

    fix fillmurray url

--- app/controllers/email_controller.rb
+++ app/controllers/email_controller.rb
@@ -4,7 +4,7 @@ class EmailController < ApplicationController
       placeholderW = (rand(1..8) * 100).to_s
       placeholderDomains = ["placekitten.com","unsplash.it","placeimg.com",
         "placebear.com","p-hold.com","placedog.net",
-        "fillmurray.com", "loremflickr.com", "placebeard.it"]
+        "www.fillmurray.com", "loremflickr.com", "placebeard.it"]
 
       @sample_image =  "https://" + placeholderDomains.sample() + "/" + placeholderH + "/" + placeholderW
 
