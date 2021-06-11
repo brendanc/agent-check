4f1c510257386c44d7d52760d77d80f15fa97764
Brendan <brendan@Devspade.com>
2021-06-11 15:50:07 -0400

    random generate a placeholder image to make sample emails easier to differentiate by site

--- app/controllers/email_controller.rb
+++ app/controllers/email_controller.rb
@@ -1,6 +1,13 @@
 class EmailController < ApplicationController
     def index
-     
+      placeholderH = (rand(1..8) * 100).to_s
+      placeholderW = (rand(1..8) * 100).to_s
+      placeholderDomains = ["placekitten.com","unsplash.it","placeimg.com",
+        "placebear.com","p-hold.com","placedog.net",
+        "fillmurray.com", "loremflickr.com", "placebeard.it"]
+
+      @sample_image =  "https://" + placeholderDomains.sample() + "/" + placeholderH + "/" + placeholderW
+
     end
   end
   
