169d21ee02f6243b54caad955edd3cda68d481ae
Brendan <brendan@Devspade.com>
2021-06-15 19:33:20 -0400

    add link to sample email

--- app/controllers/email_controller.rb
+++ app/controllers/email_controller.rb
@@ -1,13 +1,24 @@
 class EmailController < ApplicationController
     def index
+      @sample_image =  generate_sample_image
+      @sample_link = generate_sample_link
+    end
+
+    private 
+    
+    def generate_sample_image
       placeholderH = (rand(1..8) * 100).to_s
       placeholderW = (rand(1..8) * 100).to_s
       placeholderDomains = ["placekitten.com","unsplash.it","placeimg.com",
         "placebear.com","p-hold.com","placedog.net",
         "www.fillmurray.com", "loremflickr.com", "placebeard.it"]
 
-      @sample_image =  "https://" + placeholderDomains.sample() + "/" + placeholderH + "/" + placeholderW
-
+      return "https://" + placeholderDomains.sample() + "/" + placeholderH + "/" + placeholderW
     end
+
+    def generate_sample_link
+      Link.order("RANDOM()").first
+    end
+  
   end
   
